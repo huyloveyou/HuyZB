4c80ce7841918865b0097508eedd298d24b62007
huy <269506000@qq.com>
2018-05-25 00:12:21 +0800

    初始化项目

---
+++ HuyZB/HuyZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HuyZB
+//
+//  Created by mac on 2018/5/25.
+//  Copyright © 2018年 come.huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
